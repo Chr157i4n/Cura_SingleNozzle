d335fdfcc01645cb581915f539327c62d8e43d5b
Christian <8201379+Chr157i4n@users.noreply.github.com>
2019-09-08 22:34:25 +0200

    added abillity to delete further M104 and M109
    Cura set the temperature when switching extruders.
    This is not necessary for a single nozzle and prevents changing the temperature manually while printing.

--- SingleNozzle.py
+++ SingleNozzle.py
@@ -1,6 +1,6 @@
 # Cura SingleNozzle
-# Author:   Christian Köhlke
-# Date:     July 12, 2019
+# Author:   Christian Koehlke
+# Date:     September 08, 2019
 #
 # a postprocessing script for cura to use a single Nozzle
 #
@@ -32,14 +32,21 @@ class SingleNozzle(Script):
                 "active":
                 {
                     "label": "SingleNozzle",
-                    "description": "When enabled, the script is enabled",
+                    "description": "When enabled, the script is enabled. Changes every M109 to M104. deletes all M106 and M104 in terms of non existing heaters T1,T2... deletes all lines where heater0 is set to 0",
                     "type": "bool",
                     "default_value": true
                 },
                 "deleteFirstM109":
                 {
-                    "label": "delete first M109",
+                    "label": "change first M109",
                     "description": "When enabled, the script will also change the first M109 to M104. Which should be used when heating the nozzle before the print. This value should be set on false",
+                    "type": "bool",
+                    "default_value": false
+                },
+                "deleteM109M104":
+                {
+                    "label": "delete further M109 M104",
+                    "description": "When enabled, the script will also delete further M106 and M104 starting from layer 2. Cura will change the temperature of heater0 to its stanby-temperature. this will stop it.",
                     "type": "bool",
                     "default_value": false
                 }
@@ -50,6 +57,7 @@ class SingleNozzle(Script):
         
         active = self.getSettingValueByKey("active")
         deleteFirstM109 = self.getSettingValueByKey("deleteFirstM109")
+        deleteM109M104 = self.getSettingValueByKey("deleteM109M104")
         
         first_Layer=0
         last_Layer=len(data)-1
@@ -70,7 +78,7 @@ class SingleNozzle(Script):
                     for line in lines:
                         line_Index = lines.index(line)
                     
-                        if (line.startswith("M109")):                                                   #Wait for temperature in midprint
+                        if (line.startswith("M109")):                                                           #Wait for temperature in midprint
                             if ((deleteFirstM109 == False) and (firstM109 == False)):
                                 firstM109=True
                             else:
@@ -78,20 +86,25 @@ class SingleNozzle(Script):
                                 line = line.replace("M109","M104")
                                 lines.insert(line_Index,line)
                         
-                        if ((line.startswith("M109")) or (line.startswith("M104"))):                    #Non existing Heater
+                        if ((line.startswith("M109")) or (line.startswith("M104"))):                            #Non existing Heater
                             T_Index=line.find("T")
                             if (T_Index>=0): 
                                 if (line[T_Index+1].isdigit()):
                                     if (int(line[T_Index+1]) != 0):  
                                         lines.remove(line)
-                                        line="; removed Hotendtemperature for non existing Heater"
+                                        line="; removed M104 or M109 Hotendtemperature for non existing Heater"
                                         lines.insert(line_Index,line)
                         
-                        if (line.startswith("M104 T0 S0") or line.startswith("M109 T0 S0")):            #Prevent Heater shutdown on T0
+                        if (line.startswith("M104 T0 S0") or line.startswith("M109 T0 S0")):                    #Prevent Heater shutdown on T0
                             lines.remove(line)
                             line="; removed Hotendtemperature S0 for T0"
                             lines.insert(line_Index,line)
-                          
+
+                        if ( (line.startswith("M109")) or (line.startswith("M104")) ) and (layer_Index>2) and (deleteM109M104):      #delete further M109 and M104
+                            lines.remove(line)
+                            line="; removed M104 or M109 Hotendtemperature"
+                            lines.insert(line_Index,line)
+                            
                         
                 result = "\n".join(lines)
                 data[layer_Index] = result
